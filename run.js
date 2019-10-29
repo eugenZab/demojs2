ef65c4c8c3af431816f3a6b4525b5dd9875321b1
EugeneZ <eugenez@invenco.com>
2019-10-30 11:52:40 +1300

    commit 2

--- run.js
+++ run.js
@@ -1,20 +1,25 @@
 const fs = require('fs');
+const _ = require('lodash');
 const parkingObj = require("./data/parking");
 
 const addNewParkingObj = function (newParkingObj) {
-    setTimeout(() => {
+    setTimeout(function (){
         parkingObj.push(newParkingObj);
     }, 1000);
 }
 
+const deleteParkingObj = function (parkingId) {
+    setTimeout(function (){
+        _.remove(parkingObj, function(obj) {
+            return obj.parkingId === parkingId;
+          });
+    }, 1000);
+}
+
 addNewParkingObj({ car: 'Toyota Prado', owner: 'Mike Jognson', rego: 'MJD123', parkingId: 'P7' });
+deleteParkingObj("P4");
+
 
 console.log(parkingObj);
-
-
-/*setTimeout(()=>{
-    console.log(parkingObj);
-},2000);*/
-
 
 
